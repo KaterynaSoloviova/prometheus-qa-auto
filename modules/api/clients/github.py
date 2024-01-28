41f9f642eb49dcb2d691b05d8e9b5e4f6c8f5432
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2024-01-28 20:22:13 +0100

    Add individual tests for github api

--- modules/api/clients/github.py
+++ modules/api/clients/github.py
@@ -8,6 +8,7 @@ class GitHub:
         body = r.json()
         
         return body
+    
 
     def search_repo(self, name):
         r = requests.get("https://api.github.com/search/repositories", params={"q": name})
@@ -15,7 +16,27 @@ class GitHub:
         
         return body
 
+    def get_emojis(self):
+        r = requests.get(f"https://api.github.com/emojis")
+        body = r.json()
+        
+        return body
 
+    def get_commits(self, owner_name, repo_name):
+        r = requests.get(f"https://api.github.com/repos/{owner_name}/{repo_name}/commits")
+        body = r.json()
+        
+        return body
 
+    def get_code_frequency(self, owner_name, repo_name):
+        r = requests.get(f"https://api.github.com/repos/{owner_name}/{repo_name}/stats/code_frequency")
+        body = r.json()
+        
+        return body
 
+    def get_branches(self, owner_name, repo_name):
+        r = requests.get(f"https://api.github.com/repos/{owner_name}/{repo_name}/branches")
+        body = r.json()
+        
+        return body
     
--- tests/api/test_github_api.py
+++ tests/api/test_github_api.py
@@ -31,7 +31,50 @@ def test_repo_with_singlt_char_be_found(github_api):
     r = github_api.search_repo("s")
     assert r["total_count"] != 0
  
+@pytest.mark.api
+def test_check_available_emojis(github_api):
+    r = github_api.get_emojis()
+    assert r["8ball"] == "https://github.githubassets.com/images/icons/emoji/unicode/1f3b1.png?v8"
+    assert len(r) == 1877
 
+@pytest.mark.api
+def test_check_commits_in_existing_repo(github_api):
+    r = github_api.get_commits("KaterynaSoloviova", "prometheus-qa-auto")
+    assert r[0]["commit"]["message"] == "Add github api tests"
+    assert r[0]["commit"]["author"]["name"] == "Kateryna Soloviova"
+    assert len(r) >= 4
+    
+@pytest.mark.api
+def test_check_commits_in_non_existing_repo(github_api):
+    r = github_api.get_commits("KaterynaSoloviova", "non_existing_repo")
+    assert r["message"] == "Not Found"
+   
+@pytest.mark.api
+def test_check_commits_at_non_existing_user_and_repo(github_api):
+    r = github_api.get_commits("non_existing_name", "non_existing_repo")
+    assert r["message"] == "Not Found"
+    
+@pytest.mark.api
+def test_check_commits_in_private_repo(github_api):
+    r = github_api.get_commits("KaterynaSoloviova", "prometheus-python")
+    assert r["message"] == "Not Found"
+
+@pytest.mark.api
+def test_check_code_frequency_in_new_repo(github_api):
+    r = github_api.get_code_frequency("KaterynaSoloviova", "prometheus-qa-auto")
+    assert len(r) >= 0
+    
+@pytest.mark.api
+def test_check_code_frequency_in_repo_with_more_than_10000_commits(github_api):
+    r = github_api.get_code_frequency("zed-industries", "zed")
+    assert r["message"] == "repository must have fewer than 10000 commits"
+
+@pytest.mark.api
+def test_check_(github_api):
+    r = github_api.get_branches("KaterynaSoloviova", "prometheus-qa-auto")
+    assert r[0]["name"] == "main"
+    assert len(r) == 1
+    
 
 
     
